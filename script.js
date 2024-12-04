3314dfe1d3593da791bd33e5ff445bbf4ea23491
maxovsee <80675065+maxovsee@users.noreply.github.com>
2024-12-04 15:53:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 // Function to load property data from subfolders
 function loadProperties() {
-  const propertiesFolder = "properties/";
+  const propertiesFolder = './properties/';
   const propertiesContainer = document.getElementById("properties");
-
-  console.log("Loading properties from:", propertiesFolder);
 
   // Get a list of subfolders in the properties folder
   fetch(propertiesFolder)
     .then(response => response.text())
     .then(data => {
-      console.log("Subfolders:", data);
-
       const subfolders = data.match(/<a href="([^"]+)\/">/g);
 
       // Loop through each subfolder and load the property data
@@ -18,20 +14,14 @@ function loadProperties() {
         const subfolderName = subfolder.match(/<a href="([^"]+)\/">/)[1];
         const propertyDataUrl = propertiesFolder + subfolderName + "/index.json";
 
-        console.log("Loading property data from:", propertyDataUrl);
-
         fetch(propertyDataUrl)
           .then(response => response.json())
           .then(propertyData => {
-            console.log("Property data:", propertyData);
-
             // Load images from the subfolder
             const imagesUrl = propertiesFolder + subfolderName + "/";
             fetch(imagesUrl)
               .then(response => response.text())
               .then(data => {
-                console.log("Images:", data);
-
                 const images = data.match(/<a href="([^"]+\.jpg|[^"]+\.png|[^"]+\.jpeg)">/g);
 
                 // Create an image gallery
